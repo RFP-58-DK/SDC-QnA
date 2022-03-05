be898e32a19a4ea5e192abc29172874bfe6bcf69
Kenneth Wu <kunhan419.wu@gmail.com>
2022-03-06 07:57:48 +0800

    fixing query string

--- server/model/index.js
+++ server/model/index.js
@@ -1,8 +1,18 @@
 const db = require('../db');
 
 const getQuestions = (params, callback) => {
+  const queryObj = `SELECT * FROM questions LEFT OUTER JOIN answers ON questions.question_id = answers.question_id WHERE questions.product_id = ${params.product_id} LIMIT ${params.count || 5}`;
 
-  const queryStr = `SELECT * FROM questions LEFT OUTER JOIN answers ON questions.question_id = answers.question_id WHERE questions.product_id = ${params.product_id} LIMIT ${params.count || 5}`;
+  const queryStr = `
+    SELECT
+      q.question_id,
+      q.question_body,
+      q.question_date,
+      q.asker_name,
+      q.question_helpfulness,
+      q.question_reported AS reported,
+      json_build_object('1234', json_build_object('id', a.answer_id)) AS answers
+    FROM questions AS q LEFT OUTER JOIN answers AS a ON q.question_id = a.question_id WHERE q.product_id = ${params.product_id} LIMIT ${params.count || 5}`;
   db.query(queryStr,  (err, results) => {
     if (err) {
       callback(err);
@@ -37,6 +47,3 @@ module.exports = {
   getAnswers,
   getPhotos
 }
-
-
-// `SELECT * FROM questions LEFT OUTER JOIN answers ON questions.question_id = answers.question_id WHERE questions.product_id = ${params.product_id} LIMIT ${params.count || 5}`
